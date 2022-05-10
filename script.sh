389c4cc6d59dbdee90193d260c0d26f974560a3b
Ramaniuk <240724oi@gmail.com>
2022-05-09 22:27:50 -0400

    dz_1

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir hw_1
+cd hw_1
+mkdir test1 test2 test3
+cd test1
+touch text1.txt text2.txt text3.txt jn1.json jn2.json jn3.json
+mkdir test4 test5 test6
+ls -la
+mv text3.txt test4
+mv text1.txt test4
